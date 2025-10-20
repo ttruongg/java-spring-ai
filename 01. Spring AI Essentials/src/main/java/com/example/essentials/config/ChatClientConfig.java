ed4137e12abc04f0b351c99f182b6d723fa82fbe
truongdt <thanhtruong1901@gmail.com>
2025-10-20 13:44:46 +0700

    prompts, advisors and structured responses

--- 01. Spring AI Essentials/src/main/java/com/example/essentials/config/ChatClientConfig.java
+++ 01. Spring AI Essentials/src/main/java/com/example/essentials/config/ChatClientConfig.java
@@ -1,15 +1,27 @@
 package com.example.essentials.config;
 
+import com.example.essentials.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        ChatOptions chatOptions = ChatOptions.builder()
+                .model("gpt-4o")
+                .maxTokens(100)
+                .build();
+
         return chatClientBuilder
+//                .defaultOptions(chatOptions)
+                .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
                 .defaultSystem("""
                         You are an internal HR assistant. Your role is to help\\s
                         employees with questions related to HR policies, such as\\s
---
+++ 01. Spring AI Essentials/src/main/java/com/example/essentials/controller/PromptTemplateController.java
@@ -0,0 +1,40 @@
+package com.example.essentials.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PromptTemplateController {
+
+    private final ChatClient chatClient;
+
+    public PromptTemplateController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @Value("classpath:/promptTemplates/userPromptTemplate.st")
+    Resource userPromptTemplate;
+
+    @GetMapping("/email")
+    public String emailResponse(@RequestParam("customerName") String customerName,
+                          @RequestParam("customerMessage") String customerMessage) {
+
+        return chatClient
+                .prompt()
+                .system("""
+                        You are a professional customer service assistant which helps drafting email
+                        responses to improve the productivity of the customer support team
+                        """)
+                .user(promptUserSpec ->
+                        promptUserSpec.text(userPromptTemplate)
+                                .param("customerName", customerName)
+                                .param("customerMessage", customerMessage))
+                .call().content();
+    }
+}
