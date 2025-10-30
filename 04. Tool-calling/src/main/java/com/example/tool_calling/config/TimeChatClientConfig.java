14541f060d01adf1aecd88fde293d18c2f6bfb04
truongdt <thanhtruong1901@gmail.com>
2025-10-30 14:20:53 +0700

    Initialize project and basic configuration

---
+++ 04. Tool-calling/src/main/java/com/example/tool_calling/config/TimeChatClientConfig.java
@@ -0,0 +1,18 @@
+package com.example.tool_calling.config;
+
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TimeChatClientConfig {
+
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .build();
+    }
+}
---
+++ 04. Tool-calling/src/main/java/com/example/tool_calling/tools/TimeTools.java
@@ -0,0 +1,7 @@
+package com.example.tool_calling.tools;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeTools {
+}
